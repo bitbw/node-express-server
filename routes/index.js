b8d677d2a3c8344d6f92cc05f4d706f5a17e7120
张博文 <18211132604@163.com>
2022-05-09 12:29:31 +0800

    test

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Bitbw' });
 });
+router.get('/tset', function(req, res, next) {
+ res.send("test")
+});
 
 module.exports = router;
